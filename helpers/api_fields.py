7e35c96e36561e5b34d284a326e8789cc52bd8da
JhonasV <jhonas@gmail.com>
2021-08-19 00:05:28 -0400

    feat: routes registered in main file and api fields updated with new field

--- helpers/api_fields.py
+++ helpers/api_fields.py
@@ -1,2 +1,2 @@
 event_fields = ['id', 'name', 'active', 'company_id', 'organizer_id', 'kanban_state_label', 'date_begin', 
-    'date_end', 'country_id', 'event_logo']
+    'date_end', 'country_id', 'event_logo', 'is_published']
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from routes import events
+from routes import events, countries, companies
 
 app = FastAPI()
 app.include_router(events.router)
+app.include_router(countries.router)
+app.include_router(companies.router)
